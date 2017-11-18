762b49f5eae6aa3f88c35d32f540758751009ed1
Your Name <you@example.com>
2017-11-18 03:08:35 +0000

    evolve: add jobs

--- codecamp/codecamp/settings.py
+++ codecamp/codecamp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'jobs'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+ALLOWED_HOSTS = ['192.168.99.100']
